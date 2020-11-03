f189e82a8ab2d09dd2ef1ddec7391d9585f18b76
Joseph Ellis <josephtomellis@gmail.com>
2020-11-03 15:48:49 -0600

    attampt at simple acronym generatot

--- AcronyMaker/Program.cs
+++ AcronyMaker/Program.cs
@@ -6,7 +6,17 @@ namespace AcronyMaker
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter an Phrase to generate an acronym:");
+
+            string input = Console.ReadLine();;
+            
+            string[] words =  input.Split(' ');
+            char[] letters = new char[words.Length];
+            for (int i = 0; i < words.Length; i++)
+            {
+                letters[i] = char.ToUpper(words[i][0]);
+                Console.Write(letters[i]+" ");
+            }
         }
     }
 }
