ba8cb4aefb0ea1e73f3f665d2eac34e0071d3aa3
Joseph Ellis <josephtomellis@gmail.com>
2020-11-04 01:25:36 -0600

    commit of practice excercise

---
+++ ProceduralBasics/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ProceduralBasics
+{
+    // in a console app project (this template)
+    // the runtime looks for a main method to enter the program
+
+    //projects contain classes
+    //classes contain member , e.g. methods, variables.
+    //   (a method is a sequence of statements which may have input parameters and one output)
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //variable in C# with types
+            // a variable is a slot that can contain a value
+            
+            //in .NET strings are stored in unicode
+            double x = 1/3; // answer will be the same as integer division since the division is performed before assignment to a double type (i.e. 0)
+
+            Console.WriteLine("enter a number: ");
+            string input = Console.ReadLine();
+            int number = int.Parse(input);
+                //switch statement example
+                //look as switch expressions/ pattern matching
+            switch (number){
+                case 4:
+                    x=8;
+                    break;
+                case 7:
+                    x =5;
+                    break;
+            }
+            // in C# theres no primitives that are not objects, possibly excepting null
+            //"literal" refers to a value that is specified within the code.
+        }
+    }
+}
---
+++ classes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("JT", 1000);
+
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+
+            account.MakeWithdrawal(300, DateTime.Now, "walking around money");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(500, DateTime.Now,"getting paid");
+            Console.WriteLine(account.Balance);
+
+            Console.WriteLine(account.GetAccountHistory());
+            /*
+            //test for positive balance
+            try{
+                var invalidAccount = new BankAccount("invalid", -55);
+            }
+            catch(ArgumentOutOfRangeException e){
+                Console.WriteLine("exception caught creating an account with negative balance");
+                Console.WriteLine(e.ToString());
+            }
+            */
+        }
+    }
+}
