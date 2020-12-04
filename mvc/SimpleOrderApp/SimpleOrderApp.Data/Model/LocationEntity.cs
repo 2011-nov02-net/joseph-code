1fd36dbf3e1ea49350b2383e31db1adf3aa02116
Joseph Ellis <josephtomellis@gmail.com>
2020-12-04 14:08:48 -0600

    added sample mvc

---
+++ mvc/SimpleOrderApp/SimpleOrderApp.Data/Model/LocationEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace SimpleOrderApp.Data.Model
+{
+    public class LocationEntity
+    {
+        // automatically become the primary key
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Stock { get; set; }
+
+        public List<OrderEntity> Orders { get; set; } // navigation property
+    }
+}
---
+++ mvc/SimpleStore/SimpleStore.DataModel/Model/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleStore.DataModel.Model
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+
+        public int Quantity { get; set; }
+
+        //public int LocationId { get; set; }
+
+        public Location Location { get; set; }
+    }
+}
